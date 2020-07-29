8c0874ba2139f315675f317d74fa0ecd86a86831
Scoticus <scoticus@users.noreply.github.com>
2020-07-29 13:31:29 +0100

    Add questions

--- data/questions.js
+++ data/questions.js
@@ -7,14 +7,272 @@ module.exports = [
   },
   {
     id: 2,
-    title: 'How tall must a Scottish mountain be to be classified as a Munro?',
+    title: 'Munros are mountains in Scotland taller than?',
     correct: '3,000 ft',
     options: ['2,500 ft', '2,750 ft', '3,000 ft', '3,500 ft'],
   },
   {
-    id: 6,
+    id: 3,
     title: 'Which river is the longest in Scotland?',
     correct: 'River Spey',
     options: ['River Forth', 'River Dee', 'River Tweed', 'River Spey'],
+  },
+  {
+    id: 4,
+
+    title: 'Which Scottish island is home to the Cuillin range?',
+
+    correct: 'Isle of Skye',
+
+    options: ['Isle of Skye', 'Isle of Lewis', 'Isle of Arran', 'Isle of Mull'],
+  },
+
+  {
+    id: 5,
+    title: 'The Scottish Parliament was established in what year?',
+
+    correct: '1999',
+
+    options: ['1994', '1997', '1999', '2001'],
+  },
+
+  {
+    id: 6,
+
+    title: 'In what year did the Battle of Culloden take place?',
+
+    correct: '1746',
+
+    options: ['1746', '1751', '1775', '1801'],
+  },
+
+  {
+    id: 7,
+
+    title: 'What is the oldest university in Scotland?',
+
+    correct: 'University of St Andrews',
+
+    options: [
+      'University of Glasgow',
+
+      'University of St Andrews',
+
+      'University of Edinburgh',
+
+      'University of Aberdeen',
+    ],
+  },
+
+  {
+    id: 8,
+    title: 'What is the national flower of Scotland?',
+
+    correct: 'Thistle',
+
+    options: ['Thistle', 'Rose', 'Orchid', 'Carnation'],
+  },
+
+  {
+    id: 9,
+
+    title: 'The band The Proclaimers is most famous for which song?',
+
+    correct: "I'm Gonna Be (500 Miles)",
+
+    options: [
+      'Flower of Scotland',
+
+      'Auld Lang Syne',
+
+      "Donald Where's Your Troosers?",
+
+      "I'm Gonna Be (500 Miles)",
+    ],
+  },
+
+  {
+    id: 10,
+
+    title: 'The Clyde is internationally renowned for which industry?',
+
+    correct: 'Shipbuilding',
+
+    options: ['Oil & gas', 'Financial services', 'Shipbuilding', 'Fishing'],
+  },
+
+  {
+    id: 11,
+
+    title: 'What is the total length of the Forth Rail Bridge?',
+
+    correct: '2,467 m (8,094 ft)',
+
+    options: [
+      '2,124 m (6,969 ft)',
+
+      '2,467 m (8,094 ft)',
+      '2,646 m (8,681 ft)',
+      '2,699 m (8,855 ft)',
+    ],
+  },
+  {
+    id: 12,
+    title: 'What is Alexander Graham Bell credited with inventing?',
+    correct: 'Telephone',
+    options: ['Telephone', 'Penecillin', 'Television', 'Steam engine'],
+  },
+  {
+    id: 13,
+    title: 'Edinburgh Castle is home to Mons Meg, but what is it?',
+    correct: 'Cannon',
+    options: ['Turret', 'Statue', 'Cannon', 'Portcullis'],
+  },
+  {
+    id: 14,
+    title: 'What time of year does Hogmanay refer to?',
+    correct: 'New Year',
+    options: ['Easter', 'Midsummer', 'Halloween', 'New Year'],
+  },
+
+  {
+    id: 15,
+    title: 'The city of Aberdeen is famous for which type of stone?',
+    correct: 'Granite',
+    options: ['Sandstone', 'Granite', 'Limestone', 'Marble'],
+  },
+  {
+    id: 16,
+    title:
+      'The novels Treasure Island, Kidnapped, and Strange Case of Dr Jekyll and Mr Hyde were written by which Scottish author',
+    correct: 'Robert Louis Stevenson',
+    options: [
+      'Robert Louis Stevenson',
+      'Walter Scott',
+      'Robert Burns',
+      'Arthur Conan Doyle',
+    ],
+  },
+  {
+    id: 17,
+    title: 'Burke and Hare are famous for what?',
+    correct: 'Murdering people to sell their bodies for research',
+    options: [
+      'Playing the first recorded round of golf',
+      'Building the Forth Rail Bridge',
+      'Murdering people to sell their bodies for research',
+      'Releasing the Loch Ness monster',
+    ],
+  },
+  {
+    id: 18,
+    title:
+      'The Glenfinnan Viaduct has become famous from the Harry Potter films, but how many arches does it have?',
+    correct: '21',
+    options: ['14', '17', '20', '21'],
+  },
+  {
+    id: 19,
+    title:
+      'Princes Street Gardens in Edinburgh was originally a loch, what was it called?',
+    correct: 'Nor Loch',
+    options: ['Princes Loch', 'Nor Loch', 'Waverley Loch', 'Carlton Loch'],
+  },
+  {
+    id: 20,
+    title: 'Jack and Victor are the protagonists of which Scottish TV show?',
+    correct: 'Still Game',
+    options: ['Still Game', 'River City', 'Outlander', 'Monarch of the Glen'],
+  },
+  {
+    id: 21,
+    title: 'What will a Scot tell you Irn Bru is made from?',
+    correct: 'Girders',
+    options: ['Thistles', 'Haggis', 'Girders', 'Kilts'],
+  },
+  {
+    id: 22,
+    title:
+      'A criminal trial at a Scottish court have three possible verdicts. They are guilty, not guilty, and…?',
+    correct: 'Not proven',
+    options: ['Not proven', 'Not sure', 'Not known', 'Not found'],
+  },
+  {
+    id: 23,
+    title:
+      'What is the minimum length of time whisky must be aged before it can legally be called whisky?',
+    correct: '3 years',
+    options: ['6 months', '2 years', '3 years', '5 years'],
+  },
+  {
+    id: 24,
+    title: 'Which Scottish town is known as "the home of golf"?',
+    correct: 'St Andrews',
+    options: ['Fort William', 'St Andrews', 'Aviemore', 'Dumfries'],
+  },
+  {
+    id: 25,
+    title: 'What is an Arbroath Smokie?',
+    correct: 'Smoked haddock',
+    options: [
+      'Cigar manufacturer',
+      'Distillery',
+      'Steam train',
+      'Smoked haddock',
+    ],
+  },
+  {
+    id: 26,
+    title:
+      'What is the name of the fire festival held in Lerwick, Shetland each January?',
+    correct: 'Up Helly Aa',
+    options: ['Highland Games', 'Burns Night', 'Up Helly Aa', 'Samhain'],
+  },
+  {
+    id: 27,
+    title: 'On what date does Burns Night fall?',
+    correct: '25 January',
+    options: ['16 January', '25 January', '8 February', '22 March'],
+  },
+  {
+    id: 28,
+    title:
+      "The Burn's poem Tam o' Shanter culminates at which famous Scottish bridge?",
+    correct: "Brig o' Doon",
+    options: [
+      'Forth Road Bridge',
+      "Brig o' Doon",
+      "Brig o' Balgownie",
+      "Brig o' Turk",
+    ],
+  },
+  {
+    id: 29,
+    title:
+      'Scotland has the joint oldest football club in the world, but who did they play their first international match against in 1872?',
+    correct: 'England',
+    options: ['England', 'Wales', 'Ireland', 'France'],
+  },
+  {
+    id: 30,
+    title: 'Which stadium is home to the Scottish Rugby Union?',
+    correct: 'Murrayfield',
+    options: [
+      'Hampden Park',
+      'Tannadice Park',
+      'Caledonian Stadium',
+      'Murrayfield',
+    ],
+  },
+  {
+    id: 31,
+    title: 'What does the Samhain festival celebrate?',
+    correct: 'The dark half of the year',
+    options: [
+      "The defeat of Mor'du",
+      'Bottling of whisky',
+      'The dark half of the year',
+      'The Loch Ness monster',
+    ],
   },
 ];
