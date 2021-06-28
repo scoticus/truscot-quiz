659ba8b052f81ad00d7567256b577ab290decace
Scoticus <scoticus@users.noreply.github.com>
2021-06-28 17:53:08 +0100

    Add Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
 import Head from 'next/head';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
+
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  process.env.NODE_ENV === 'production' &&
+    useEffect(() => {
+      Fathom.load('LNQHODLI', {
+        url: 'https://reptile.truscot.com/script.js',
+      });
+
+      function onRouteChangeComplete() {
+        Fathom.trackPageview();
+      }
+
+      router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+      return () => {
+        router.events.off('routeChangeComplete', onRouteChangeComplete);
+      };
+    }, []);
+
+
   return (
     <>
       <Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <html lang="en">
+      <Html lang="en">
         <Head>
           <link
             rel="icon"
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
